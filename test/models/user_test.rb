df1b8e3658e393217d95f698fb6c04e5d481c9a4
Grzegorz <gb87@wp.pl>
2015-08-01 21:00:42 +0200

    Use SSL and the Puma webserver in production

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
  def setup
     @user = User.new(name: "Example User", email: "user@example.com", 
-      password: "foobar", password_confirmation: "foobar")
+      password: "foobar1", password_confirmation: "foobar1")
 
       end
 

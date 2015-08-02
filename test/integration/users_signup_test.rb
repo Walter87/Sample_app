76f3b31300ff1b92b3ef1f51935ce1e9cda97ab9
Grzegorz <gb87@wp.pl>
2015-08-02 09:56:19 +0200

    Modifications after Chapter 7 Exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	                           password_confirmation: "bar" }
 	  end
 	  assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
 	end
   test "valid signup information" do
   		get signup_path
@@ -23,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   												 password_confirmation: "password" }
   		end
   		assert_template 'users/show'
+      assert_not flash.empty?
   	end
 end
